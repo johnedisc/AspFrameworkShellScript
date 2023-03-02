4be851d3fed740c31a47d202413d741479618421
BrentHickman <BrentHickman@ymail.com>
2023-03-02 13:50:03 -0800

    find solution for bad sed command. use -i .bak then rm
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- buildMacOSASPTree.sh
+++ buildMacOSASPTree.sh
@@ -8,29 +8,29 @@ mkdir -p $1.Tests/ModelTests
 #Generate Gitigonore and Readme
 touch .gitignore README.md
 cd $1/
-printf 'bin\nobj\n' >> .gitignore
+printf "bin\nobj\n" >> .gitignore
 
 #Generate Program.cs
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Program.cs
-sed -i 's/ToDoList/$1/g' Program.cs
+sed -i "s/ToDoList/$1/g" Program.cs
 #Generate csproj
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/ToDoList.csproj
 mv ToDoList.csproj $1.csproj
 #Generate HomeController
 cd Controllers/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Controllers/HomeController.cs
-sed -i 's/ToDoList/$1/g' HomeController.cs
+sed -i "s/ToDoList/$1/g" HomeController.cs
 #Generate Sample Model
 cd ../Models
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Models/Item.cs
 mv Item.cs SampleModel.cs
-sed -i 's/ToDoList/$1/g' SampleModel.cs
+sed -i "s/ToDoList/$1/g" SampleModel.cs
 #Generate Home View
 cd ../Views
 mkdir Home/
 cd Home/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Views/Home/Index.cshtml
-sed -i 's/My To-Do List!//g' Index.cshtml
+sed -i "s/My To-Do List!//g" Index.cshtml
 #Generate Shared Folder
 cd ../
 mkdir Shared
@@ -42,13 +42,14 @@ cd ../
 cd ../$1.Tests
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList.Tests/ToDoList.Tests.csproj
 mv ToDoList.Tests.csproj $1.Tests.csproj
-sed -i 's/ToDoList/$1/g' $1.Tests.csproj
+sed -i .bak "s/ToDoList/$1/g" $1.Tests.csproj
 
 #Generate ModelTest Sample
 cd ModelTests/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList.Tests/ModelTests/ItemTests.cs
 mv ItemTests.cs SampleModelTests.cs
-sed -i 's/ToDoList/$1/g' 'SampleModelTests.cs'
+sed -i .bak "s/ToDoList/$1/g" SampleModelTests.cs
+rm SampleModelTests.cs.bak
 
 
 
