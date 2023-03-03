69c39b80ba386e5fbb697feb9350ae08836755e7
johnedisc <christopher.johnedis@gmail.com>
2023-03-02 23:22:52 -0800

    add 2nd argument to determine target destination for the directory. add error handling

--- buildASPTree.sh
+++ buildASPTree.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
 
+if [ $# != 2 ]; then
+  printf "please enter two arguments: \n the first is the ProjectName in Pascal Notation \n the second is the path where you want to store the project directory \n"
+  exit 1
+fi
+
+cd $2
 #Generate Directories
 mkdir -p $1.solution/$1/Controllers/ $1.solution/$1/Properties/ $1.solution/$1/Models/ $1.solution/$1/Views/ $1.solution/$1/wwwroot/
 cd $1.solution/
@@ -13,27 +19,33 @@ cd $1/
 #Generate Program.cs
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Program.cs
 sed -i "s/ToDoList/$1/g" Program.cs
+
 #Generate csproj
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/ToDoList.csproj
 mv ToDoList.csproj $1.csproj
+
 #Generate HomeController
 cd Controllers/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Controllers/HomeController.cs
 sed -i "s/ToDoList/$1/g" HomeController.cs
+
 #Generate launch.json file
 cd Properties/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Properties/launchSettings.json
+
 #Generate Sample Model
 cd ../Models
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Models/Item.cs
 mv Item.cs SampleModel.cs
 sed -i "s/ToDoList/$1/g" SampleModel.cs
+
 #Generate Home View
 cd ../Views
 mkdir Home/
 cd Home/
 wget https://raw.githubusercontent.com/BrentHickman/ToDoList.Solution/main/ToDoList/Views/Home/Index.cshtml
 sed -i "s/My To-Do List!//g" Index.cshtml
+
 #Generate Shared Folder
 cd ../
 mkdir Shared
